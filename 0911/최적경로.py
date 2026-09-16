8230e73d2c1f4604b141088cf2168a1c1d4eb893
김동준 <jkfdgd1114@naver.com>
2026-09-16 17:42:05 +0900

    경로 푸는중

---
+++ 0911/최적경로.py
@@ -0,0 +1,51 @@
+def dist(A, B): # 점 A와 B의 거리 반환
+    return abs(A[0]-B[0])+abs(A[1]-B[1])
+
+def visit(now): # customers로 만들 수 있는 길이 N의 순열의 가짓수
+    global ans
+    if len(route) == N-1:
+        localsum = 0
+        for i in range(0, N-2): # 01, 12, 23, 34 ... 910
+            localsum += dist(route[i], route[i+1])
+        localsum += dist(route[-1], home)
+        if ans > localsum:
+            ans = localsum
+        return 
+    for j in range(j, N+1):
+        route.append(j)
+        visit(now+1)
+        route.pop()
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    work = [arr[0], arr[1]]
+    home = [arr[2], arr[3]]
+    customers = [] # 길이 : N
+    for i in range(4, N*2+3, 2): # 4 6 8 10 ... N*2 N*2+2
+        customers.append([arr[i], arr[i+1]])
+    route = [work] # 길이 : N+1 (마지막에 home이랑 거리잴거임)
+
+    ans = 99999
+    print(f'#{tc} {ans}')
+
+
+"""
+3
+5
+0 0 100 100 70 40 30 10 10 5 90 70 50 20
+6
+88 81 85 80 19 22 31 15 27 29 30 10 20 26 5 14
+10
+39 9 97 61 35 93 62 64 96 39 36 36 9 59 59 96 61 7 64 43 43 58 1 36
+
+1 2 3로 순열 만든다고 하면
+6가지
+123
+132
+213
+231
+312
+321
+
+"""
--- 0911/토너먼트카드게임.py
+++ 0911/토너먼트카드게임.py
@@ -1,11 +1,66 @@
-def rsp(A, B): # 가위 1 묵 2 보 3
-    if B == (A%3)+1: # 1을 이기는건 2 3을 이기는건 1
+def rsp(A, B):
+    if B[1] == (A[1] % 3) + 1:
         return B
-    else:
-        return A
+    return A
+
+
+def div(haks):
+    if len(haks) == 1:
+        return haks[0]
+    mid = (len(haks) + 1) // 2
+    left = div(haks[:mid])
+    right = div(haks[mid:])
+    return rsp(left, right)
+
 
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
-    haksang = list(map(int, input().split()))
-    # 이진탐색 스택 잘 쓰면 풀릴듯
+    cards = list(map(int, input().split()))
+    haksang = []
+    for idx, card in enumerate(cards):
+        haksang.append([idx, card])
+    ans = div(haksang)
+    print(f'#{tc} {ans[0] + 1}')
+
+
+
+"""
+3
+4
+1 3 2 1
+6
+2 1 1 2 3 3
+7
+1 3 3 3 1 1 3
+
+밑으로 내려가다보면 2개로 떨어지거나 3개로 떨어지거나 둘중하난데 DP로 풀수있을거같음
+
+한쪽이 3으로 나뉠때 함수를 만들어놓으면 편할거같음(그럴필요없을수도)
+
+N4 = 2, 2
+N5 = 3, 2
+N6 = 3, 3
+N7 = 3, N4
+N8 = N4, N4
+N9 = N4, N5
+...
+N13 = N6, N7
+...
+N25 = N12, N13
+...
+N50 = N25, N25
+...
+N100 = N50, N50
+
+아마 최대 8번 나눌듯? 대충 N 범위에 따라 log2(N)+2 정도로 temp 길이 조정하면될것으로보임
+
+근데 뭐가 우승하는진 아는데 얘가 원래 몇번이었는지 알수가없음 지금
+
+맨처음에 각각 학생마다 인덱스를 붙여서 같이넘겨줘야할듯
+
+9 9 9 9 9 9 9 9 9
+
+
+
+"""
